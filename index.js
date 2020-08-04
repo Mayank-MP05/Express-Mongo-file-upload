86547d22f8c58dab648f4703bb2ee035af71c893
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-04 13:55:30 +0530

    File Upload Logic

--- index.js
+++ index.js
@@ -7,30 +7,22 @@ const path = require("path");
 const PORT = 5000 || process.env.PORT;
 const multer = require("multer");
 const fs = require("fs");
+const uuid = require("uuid");
 const upload = multer({ dest: "uploads/" });
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-//Simple Get Request
 
-const mid1 = (req, res, next) => {
-  console.log("Time:", new Date().toLocaleString());
-  console.log("moddlewware called");
-  next();
-};
 
-app.get("/about", mid1, (req, res) => {
-  res.json({ user: "MAyank" });
-});
 
-let loggein = true;
-app.get("/login", (req, res) => {
-  if (loggein) {
-    res.cookie("first", "0");
-    res.sendFile(path.join(__dirname, "public/home.html"));
-  } else {
-    res.sendFile(path.join(__dirname, "public/login.html"));
-  }
+//File upload example
+app.get("/profile", (req, res) => {
+  const file = req.file;
+  let newpath = uuid.v4();
+  fs.rename(file.path, `./uploads/${newpath}`, function (err) {
+    if (err) throw err;
+    res.sendFile(path.join(__dirname, `./uploads/${newpath}`));
+  });
 });
 
 app.listen(PORT, () => {
