efc79f5889458e77e40629c1d3b266b0bfa18350
Mayank_MP5 <mayank5pande@gmail.com>
2020-08-04 15:42:36 +0530

    .

--- index.js
+++ index.js
@@ -13,16 +13,23 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-
+app.get("/uploadfile", (req, res) => {
+  res.sendFile(path.join(__dirname, `./public/file_upload.html`));
+});
 
 //File upload example
-app.get("/profile", (req, res) => {
+app.post("/profile", (req, res) => {
   const file = req.file;
+  console.log(req.files);
   let newpath = uuid.v4();
-  fs.rename(file.path, `./uploads/${newpath}`, function (err) {
+  fs.rename(file.path, `./uploads/${newpath}.${file.mimetype}`, function (err) {
     if (err) throw err;
     res.sendFile(path.join(__dirname, `./uploads/${newpath}`));
   });
+});
+
+app.post('/upload', function(req, res) {
+  console.log(req.files.foo); // the uploaded file object
 });
 
 app.listen(PORT, () => {
